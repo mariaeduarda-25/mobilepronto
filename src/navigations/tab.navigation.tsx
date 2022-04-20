6d230bb4917940f76c4247cc8668e794a4057d2d
TobiasMaugus <tobiasmaugus705@gmail.com>
2022-04-20 08:33:44 -0300

    aula 20/04 commit2

--- src/navigations/tab.navigation.tsx
+++ src/navigations/tab.navigation.tsx
@@ -1,13 +1,28 @@
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { PerfilScreen } from "../screens";
+import colors from "../styles/colors";
+import { Ionicons } from "@expo/vector-icons";
 
 const Tab = createBottomTabNavigator();
 
 export default function TabNavigation() {
   return (
-    <Tab.Navigator>
-      <Tab.Screen name="Perfil" component={PerfilScreen} />
+    <Tab.Navigator
+      screenOptions={{
+        headerShown: false,
+        tabBarBadgeStyle: { backgroundColor: colors.purple },
+      }}
+    >
+      <Tab.Screen
+        name="Perfil"
+        component={PerfilScreen}
+        options={{
+          tabBarIcon: () => (
+            <Ionicons name="person" size={24} color={colors.purple} />
+          ),
+        }}
+      />
     </Tab.Navigator>
   );
 }
